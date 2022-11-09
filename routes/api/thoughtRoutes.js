747cfe2358457d780fdc4932b40d60decaf1a622
CBruno <christian.bruno1995@gmail.com>
2022-11-09 17:21:58 -0500

    added api routes to allow insomnia to work

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+
+const {
+    getAllThoughts,
+    getOneThought,
+    createThought,
+    deleteThought,
+    updateThought,
+    addReaction,
+    deleteReaction
+} = require('../../controllers/thoughtController')
+
+router.route('/').get(getAllThoughts).post(createThought)
+
+router.route('/:thoughtId').get(getOneThought).put(updateThought).delete(deleteThought)
+
+router.route('/:thoughtId/reation').put(addReaction)
+
+router.route('/:thoughtId/reation/:reactionId').put(deleteReaction)
+
+
+module.exports = router
