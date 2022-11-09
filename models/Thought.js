e5a88736de5fe2aec715d8526285c5648046787f
CBruno <christian.bruno1995@gmail.com>
2022-11-09 12:16:07 -0500

    added Thought and User model code

---
+++ models/Thought.js
@@ -0,0 +1,64 @@
+const { type } = require('express/lib/response')
+const { Schema, Types, model } = require('mongoose');
+
+const reactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        maxlength: 280,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+})
+
+const thoughtSchema = new Schema(
+    {
+        thoughtId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        thoughtText: {
+            type: String,
+            required: true,
+            maxlength: 280,
+            minlength: 1,
+            default: 'Untitled Thought'
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            getters: true,
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+thoughtSchema
+    .virtual('reactionCount')
+    .get(function() {
+        return this.reactions.length;
+    })
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
